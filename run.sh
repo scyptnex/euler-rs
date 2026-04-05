79e81066220f341e55b2b649a056278e06d027db
Scyptnex <nic.h.misc@gmail.com>
2026-04-05 20:03:50 +1000

    floats are faster...

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #! /usr/bin/env bash
 
-set -eux
+set -eu
 
 main()
 {
     local latest=$(ls -t src/bin | grep "^e[0-9]*.rs$" | head -n 1)
     latest="${latest%.rs}"
-    cargo test --bin "${latest}"
-    cargo run --bin "${latest}"
+    if [ $# -gt 0 -a $1 == "--time" ]; then
+        cargo build --release --bin "${latest}"
+        time ./target/release/"${latest}"
+    else
+        cargo test --bin "${latest}"
+        cargo run --bin "${latest}"
+    fi
 }
 
 main "$@"
--- src/bin/e073.rs
+++ src/bin/e073.rs
@@ -1,15 +1,21 @@
 use num::integer::gcd;
 
+// fn count_tween(den: u64) -> u64 {
+//     (2 * den + 1..3 * den)
+//         .filter(|nn| nn % 6 == 0)
+//         .map(|nn| nn / 6)
+//         .filter(|n| gcd(*n, den) == 1)
+//         .count() as u64
+// }
+
 fn count_tween(den: u64) -> u64 {
-    (2 * den + 1..3 * den)
-        .filter(|nn| nn % 6 == 0)
-        .map(|nn| nn / 6)
-        .filter(|n| gcd(*n, den) == 1)
-        .count() as u64
+    let lower = ((den as f64) / 3.0).ceil() as u64;
+    let upper = ((den as f64) / 2.0).floor() as u64;
+    (lower..=upper).filter(|n| gcd(*n, den) == 1).count() as u64
 }
 
 fn solve(n: u64) -> u64 {
-    (2..=n).map(count_tween).sum()
+    (4..=n).map(count_tween).sum()
 }
 
 fn main() {
